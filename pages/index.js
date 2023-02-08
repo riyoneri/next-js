9ed8f10fb68ace2519f92367539bd16984496c0b
riyoneri <lionkaneza@gmail.com>
2023-02-08 12:41:03 +0200

    Add links

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
+import { Fragment } from "react"
+
+import Link from 'next/link'
+
 const HomePage = () => {
-    return <h1>This is Home Page</h1>
+    return <Fragment>
+        <h1>This is Home Page</h1>
+        <ul>
+            <li>
+                <Link href="/news/next-js-is-a-great-framework">Nextjs is a great framework</Link>
+            </li>
+            <li>
+                <Link href="/news/something-else">Something else</Link>
+            </li>
+        </ul>
+    </Fragment>
 }
 
 export default HomePage
